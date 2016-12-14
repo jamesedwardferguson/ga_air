61582be50889cf55153ba0e281e974f1f1e96153
silvatim <silva.tim@gmail.com>
2016-12-14 17:00:03 +1100

    add js stuff

--- app/assets/javascripts/collections/FlightReservations.js
+++ app/assets/javascripts/collections/FlightReservations.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 var app = app || {};
 
 app.Flights = Backbone.Collection.extend({
   url: "/flights",
   model: app.Flight,
-  
+
   initialize: function(){
     console.log("A new collection was added");
 
@@ -18,23 +17,3 @@ app.Flights = Backbone.Collection.extend({
 
   }
 });
-=======
-// var app = app || {};
-//
-// app.Flights = Backbone.Collection.extend({
-//   url: "/flights",
-//   model: app.Flight,
-//   initialize: function(){
-//     console.log("A new collection was added");
-//
-//     this.on("add", function(reservation){
-//       var sv = new app.FlightView({
-//         model: flightReservation
-//       });
-//       sv.render();
-//
-//     });
-//
-//   }
-// });
->>>>>>> 9b2db8cba7af7f159dd854a53e9707a1db232078
